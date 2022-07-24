c4d2d6fddb89111738a34ce50583ae0cf6178758
ts moosavi <tt.musavi@gmail.com>
2022-07-24 20:46:08 +0430

    bind view in activity

--- app/src/main/java/com/example/flow/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/flow/ui/activity/MainActivity.kt
@@ -4,14 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.flow.R
+import com.example.flow.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         installSplashScreen()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
     }
 }

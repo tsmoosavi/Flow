62c4cb33a0013e8818c159cdbbafeb58619b36cf
Zarisa <jannesari.zarisa@gmail.com>
2022-08-13 20:16:07 +0430

    extensions for flow

--- app/src/main/java/com/example/util/extention.kt
+++ app/src/main/java/com/example/util/extention.kt
@@ -4,7 +4,6 @@ package com.example.util
 
 import android.content.Context
 import android.text.Editable
-import android.view.RoundedCorner
 import android.view.View
 import android.widget.EditText
 import android.widget.ImageView
@@ -13,18 +12,23 @@ import androidx.core.view.isGone
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.viewModelScope
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.bitmap.CircleCrop
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.example.data.network.util.Cause
+import com.example.data.network.util.NetworkResult
 import com.example.flow.R
+import com.example.flow.ui.utils.widget.LoadingView
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
 import kotlin.contracts.ExperimentalContracts
 import kotlin.contracts.contract
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
 
 fun View.visible() {
     isVisible = true
@@ -42,13 +46,25 @@ fun Fragment.showToastMessage(message: String) {
     Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
 }
 
-fun Fragment.showErrorMessage(cause:Cause?){
+fun Fragment.showErrorMessage(cause: Cause?) {
+    requireContext().showErrorMessage(cause)
+}
+
+fun Context.showErrorMessage(cause: Cause?) {
     val message = cause?.msg?.ifBlank { getString(cause.msgResId) }
         ?: getString(R.string.default_error_message)
-    Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 }
-inline fun ViewModel.launchScope(crossinline  block: suspend CoroutineScope.() -> Unit): Job {
+
+inline fun ViewModel.launchScope(crossinline block: suspend CoroutineScope.() -> Unit): Job {
     return viewModelScope.launch { block() }
+}
+
+inline fun Fragment.launchScope(
+    context: CoroutineContext = EmptyCoroutineContext,
+    crossinline block: suspend CoroutineScope.() -> Unit,
+): Job {
+    return viewLifecycleOwner.lifecycleScope.launch(context) { block() }
 }
 
 fun <T, F> ImageView.loadImage(
@@ -70,21 +86,68 @@ fun <T, F> ImageView.loadImage(
             if (placeholder != null) placeholder(placeholder)
             if (errorPicture != null) error(errorPicture)
             if (isCircular) circleCrop()
-            if (isCrossFade) transition(DrawableTransitionOptions.withCrossFade(defaultCrossFadeDuration))
+            if (isCrossFade) transition(
+                DrawableTransitionOptions.withCrossFade(
+                    defaultCrossFadeDuration
+                )
+            )
             if (isRoundedCorner) transform(RoundedCorners(defaultRoundCorner))
         }
         .into(this)
 }
 
-fun <T> T?.safe():T = checkNotNull(this)
+fun <T> T?.safe(): T = checkNotNull(this)
 
-fun <T> T?.isNotNull():Boolean{
+fun <T> T?.isNotNull(): Boolean {
     contract {
-        returns(true) implies(this@isNotNull != null)
+        returns(true) implies (this@isNotNull != null)
     }
-    return  this != null
+    return this != null
 }
 
-fun Editable?.textOrEmpty():String = this?.toString().orEmpty()
-fun EditText?.textOrEmpty():String = this?.text.textOrEmpty()
+fun Editable?.textOrEmpty(): String = this?.toString().orEmpty()
+fun EditText?.textOrEmpty(): String = this?.text.textOrEmpty()
+
+inline fun <T> Flow<NetworkResult<T>>.safeCollect(
+    loadingView: LoadingView? = null,
+    dataView: View? = null,
+    context: Context,
+    scope: CoroutineScope,
+    crossinline onRetry: () -> Unit = {},
+    crossinline onError: suspend () -> Unit = {},
+    crossinline onLoading: suspend () -> Unit = {},
+    crossinline onSuccess: suspend (data: T) -> Unit,
+//    isFading:Boolean
+): Job = scope.launch {
+    collect {
+        when (it) {
+            is NetworkResult.Error -> {
+                loadingView?.onError { onRetry() }
+                dataView?.gone() //or fadeOut if isFading==true
+                context.showErrorMessage(it.cause)
+                onError()
+            }
+            is NetworkResult.Loading -> {
+                loadingView?.onLoading()
+                dataView?.gone()
+                onLoading()
+            }
+            is NetworkResult.Success -> {
+                loadingView?.onSuccess()
+                dataView?.visible()
+                onSuccess(it.data!!)
+            }
+        }
+    }
+}
+
+inline fun <T> Flow<T>.collectOnScope(
+    scope: CoroutineScope,
+    crossinline block: suspend (data:T) -> Unit
+) = scope.launch {
+    collect {
+        block(it)
+    }
+}
+
 
